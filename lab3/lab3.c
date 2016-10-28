fa01dd321af16a22fe5c62524302d92202a8c9e0
bohdanD <doroshbohdan1@gmai.com>
2016-10-28 08:50:00 +0300

    lab3

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -23,123 +23,127 @@
 
 
 #include <stdio.h>
+#include <stdio.h>
 #include <math.h>
+#define M_PI 3.14159265358979323846
 
-void writef(char file[], int n)
+long double f(long double x)
 {
-	FILE *fp;
-	fp = fopen(file, "w+");
-	double step =  5.0/(n);
-	double x = 0;
-	double y;
-	char s1[50];
-	char s2[50];
-	for(int i=0; i<=n; i++)
+	return x*sinl(x*M_PI/180.0L);
+}
+int tabul(){
+	int N = 0, i=0;long double x0=0.0L, xn=0.0L;
+	printf("Введіть кількість вузлів:");
+	scanf("%i",&N);
+	printf("Введіть значення абсциси першого вузла:");
+	scanf("%Lf",&x0);
+	printf("Введіть значення абсциси останнього вузла:");
+	scanf("%Lf",&xn);
+	long double x[N+1],y[N+1];
+	long double h = (xn-x0)/(N-1);
+	//h*=5;
+	//табулювання функції
+	for (i = 1;i <= N; i++)
 	{
-		y = sin(x);
-		sprintf(s1, "%f", x);
-		sprintf(s2, "%f", y);
-		fputs(s1, fp);
-		fputs("\t", fp);
-		fputs(s2, fp);
-		fputs("\n", fp);
-		x += step;
+		x[i] = x0+(i-1)*h;
+		y[i] = f(x[i]);
 	}
-	fclose(fp);
+	//запис протабульованих даних у файл inputL.txt
+	FILE *finput = fopen("input.txt","w");
+	for(i=1;i<=N;i++)
+	{
+		fprintf(finput,"%i)\t%10.22Le\t%10.22Le\n",i,x[i],y[i]);
+	}
+	printf("Дані записано в input.txt");
+	fclose(finput);
+	return 0;
 }
 
-void read(char file[], double x[], double y[], int n)
+long double w(int k, long double user_x, const long double *x)
 {
-	FILE *fp;
-	char buff[255];
-	char buff2[255];
-	fp = fopen(file, "r");
-	for(int i=0; i<=n; i++)
+	int i = 0;
+	long double mult=1;
+	for (i=0;i<=k;i++)
 	{
-		
-			fscanf(fp, "%s", buff);
-		
-			fscanf(fp, "%s", buff2);
-		sscanf(buff, "%lf", &x[i]);	
-		sscanf(buff2, "%lf", &y[i]);	
-		//printf("%lf", y[i]);
+		mult*=(user_x-x[i]);
 	}
+	return mult;
 }
 
-double omega(double x, double arr[], int n)
+long double rr(int n, const long double *x, const long double *y)
 {
-	double res = 1;
-	for(int i= 0; i<n; i++)
+	int i = 0,j=0; long double sum=0,mult=1;
+	for (i=0;i<=n;i++)
 	{
-		if(x!=arr[i])
-			res *= (x - arr[i]);
+		mult=1;
+		for(j=0;j<=n;j++)
+		{
+			if (j!=i)
+			{
+				mult*=(x[i]-x[j]);
+				
+			}
+		}
+		sum+=(y[i]/mult);
 	}
-	return res;
+	return sum;
 }
 
-double newton(double x[], double y[], int n)
+long double Nn(int n, long double tmp_x, const long double *x, const long double *y)
 {
-	double res=0;
-	for(int i=0; i<=n; i++)
+	int k=0;
+	long double result = 0;
+	for(k=1;k<=n;k++)
 	{
-		//printf("\n%lf", t);
-		res+= y[i]/ omega(x[i], x, n); 
+		result+=(w(k-1,tmp_x,x)*rr(k,x,y));
 	}
-	return res;
+	result+=y[0];
+	return result;
 }
 
-double polynome(double xArg, double x[], double y[], int n)
+long double Feps(int n, long double tmp_x, const long double *x, const long double *y)
 {
-	double res = y[0];
-	for(int i=1; i<=n; i++)
-	{
-		res += omega(xArg, x, i-1)*newton(x, y, i);
-	}
-	return res;
+	return fabsl(f(tmp_x)-Nn(n,tmp_x,x,y));
 }
 
-double eps(double y, double pol)
-{
-	return fabs(y-pol);
-}
-	
-void writePolynome(char file[], double x[], double yarr[], int n)
- {
-	 FILE *fp;
-	fp = fopen(file, "w+");
-
-	double y, e;
-	char s1[50];
-	char s2[50];
-	char s3[50];
-	//int k = n/100;
-	for(int i=1; i<=n; i++)
-	{
-		y = polynome(x[i], x, yarr, 5);
-		e = eps(yarr[i], y);
-		sprintf(s1, "%f", x[i]);
-		sprintf(s2, "%f", y);
-		sprintf(s3, "%f", e);
-		fputs(s1, fp);
-		fputs("\t", fp);
-		fputs(s2, fp);
-		fputs("\t", fp);
-		fputs(s3, fp);
-		fputs("\n", fp);
-	
-	}
-	fclose(fp);
- }
 
 int main(int argc, char **argv)
 {
-	int n = 100;
-	char file[] = "res.txt";
-	double x[n+1];
-	double y[n+1];
-	writef(file, n);
-	read(file, x, y, n);
-	writePolynome("polynome.txt", x, y, n);
+	tabul();
+	FILE * fdata = fopen("input.txt","r");
+	FILE * fdata_out = fopen("output.txt","w");
+	int N = 0, n = 0, i = 0, j = 0;
+	long double xc=0,a=0,b=0,l=0,h=0;
+	char one_char;
+	//визначення кількості вузлів
+	while((one_char = fgetc(fdata)) != EOF)
+    if (one_char == '\n') ++N;
+	long double x[N+1],y[N+1];//,xm[20*N+1],ym[20*N+1],h[N+1];
+	//зчитування вхідних даних
+	rewind(fdata);
+	for(i=0;i<N;i++)
+	{
+		fscanf(fdata,"%i\t%*c\t%Le\t%Le",&j,&x[i],&y[i]);
+		//printf("%i)\t%Le\t%Le\n",j,x[i],y[i]);
+	}
+	n=N-1;
+	a=x[0];
+	b=x[n];
+	//printf("Введіть x для обчислення значення інтерполяційного многочлена Ньютона:");
+	//scanf("%Lf",&xc);
+	//printf("Nn(xc=%Lf)=%Lf\n",xc,Nn(n,xc,x,y));
+	//printf("Табуляція 6\n");
+	h=(b-a)/(long double)(20*n);
+	b=a+360*h/4.0;
+	for (l = a;l<=b;l+=h)
+	{
+		xc = l;
+		fprintf(fdata_out,"%Le\t%Le\t%Le\t%Le\t%Le\n",xc,f(xc),Nn(n,xc,x,y),Feps(n,xc,x,y),w(n,xc,x));
+		//printf("%Le\t%Le\t%Le\t%Le\t%Le\n",xc,f(xc),Nn(n,xc,x,y),Feps(n,xc,x,y),w(n,xc,x));
+	}
+	printf("Дані записано в output.txt");
+	fclose(fdata);
+	fclose(fdata_out);
 	return 0;
 }
 
