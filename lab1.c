18e2ca2cdefe84deb0ffbc2d7db36cada9574217
bohdanD <doroshbohdan1@gmai.com>
2016-12-14 10:56:36 +0200

    lab1

--- lab1.c
+++ lab1.c
@@ -24,44 +24,143 @@
 
 #include <stdio.h>
 #include <math.h>
-<<<<<<< HEAD
-	
-=======
+#define M_PI 3.14159265358979323846
 
-
-
->>>>>>> ac019925819b0d64ff31b707662964bf794b0d67
-
-void writef()
+long double f(long double x)
 {
-	FILE *fp;
-	fp = fopen("res.txt", "w+");
-	int iter = 100;
-	double step = 0.06;
-	double x = -3;
-	double y;
-	char s1[50];
-	char s2[50];
-	for(int i=0; i<iter; i++)
+	return x*sin(x*M_PI/180.0);
+}
+void tabul()
+{
+	int N = 0, i=0;long double x0=0.0, xn=0.0;
+	printf("Введіть кількість вузлів:");
+	scanf("%i",&N);
+	printf("Введіть значення абсциси першого вузла:");
+	scanf("%Lf",&x0);
+	printf("Введіть значення абсциси останнього вузла:");
+	scanf("%Lf",&xn);
+	long double x[N+1],y[N+1],h[N+1];
+	long double tmp_h = (xn-x0)/(N-1);
+	//табулювання функції
+	for (i = 1;i <= N; i++)
 	{
-		y = sin(x);
-		sprintf(s1, "%f", x);
-		sprintf(s2, "%f", y);
-		fputs(s1, fp);
-		fputs("\t", fp);
-		fputs(s2, fp);
-		fputs("\n", fp);
-		x += step;
+		x[i] = x0;
+		h[i] = tmp_h;
+		y[i] = f(x[i]);
+		x0+=tmp_h;
 	}
-	fclose(fp);
+	//запис протабульованих даних у файл inputL.txt
+	FILE *finput = fopen("input.txt","w");
+	for(i=1;i<=N;i++)
+	{
+		fprintf(finput,"%Le\t%Le\t%Le\n",x[i],y[i],h[i]);
+	}
+	printf("Дані записано в input.txt");
+	fclose(finput);
+	return;
 }
 
-
-
-int main(int argc, char **argv)
+//розв’язання тридіагональної матриці - знаходження коеф. с
+void tridiagonal_matrix_solve(long double * y,long double *h,const int N,long double *c)
 {
-	writef();
-	return 0;
-	
+	FILE *fch = fopen("check.txt","w");//файл для запису похибки матриці
+	int i = 0,n=N-1;//N - к-сть вузлів, n - номер останнього вузла
+	long double alfa[N+1],beta[N+1],hamma[N+1],delta[N+1],A[N+1],B[N+1],eps[N+1];
+	//визначення коефіцієнтів alfa,beta,hamma,delta
+	alfa[1]=0.0;beta[1]=1.0;hamma[1]=0.0;delta[1]=0.0;
+	for(i=2;i<=n;i++)
+	{
+		alfa[i]=h[i-1];
+		beta[i]=2*(h[i-1]+h[i]);
+		hamma[i]=h[i];
+		delta[i]=3*(((y[i]-y[i-1])/h[i])-((y[i-1]-y[i-2])/h[i-1]));
+	}
+	hamma[n]=0.0;
+	//прямий хід
+	//визначення коефіцієнтів A,B
+	A[1]=-hamma[1]/beta[1];
+	B[1]=delta[1]/beta[1];
+	for(i=2;i<=n;i++)
+	{
+		A[i]=-hamma[i]/(alfa[i]*A[i-1]+beta[i]);
+		B[i]=(delta[i]-alfa[i]*B[i-1])/(alfa[i]*A[i-1]+beta[i]);
+	}
+	//зворотній хід
+	c[n]=(delta[n]-alfa[n]*B[n-1])/(alfa[n]*A[n-1]+beta[n]);
+	for(i=n;i>1;i--)
+	{
+		c[i-1]=A[i-1]*c[i]+B[i-1];
+	}	
+	//обчислення різниці між правою і лівою частинами системи рівнянь
+	eps[1]=fabs(beta[1]*c[1]+hamma[1]*c[2]-delta[1]);	
+	for (i=2;i<n;i++)
+	{
+		eps[i]=fabs(alfa[i]*c[i-1]+beta[i]*c[i]+hamma[i]*c[i+1]-delta[i]);
+	}
+	eps[n]=fabs(alfa[n]*c[n-1]+beta[n]*c[n]-delta[n]);
+	//обчислення різниці в файл
+	for (i=1;i<=N;i++)
+	{
+		fprintf(fch,"%i-%Le\n",i,eps[i]);
+	}
+	fclose(fch);
 }
 
+int main()
+{
+	tabul();
+	FILE * fdata = fopen("input.txt","r");
+	int N = 0, i = 0;
+	long double x0=0.0;
+	//визначення кількості вузлів
+	while (fscanf(fdata,"%Le\t%Le\t%Le",&x0,&x0,&x0)!=EOF)
+	{
+		N++;
+	}
+	long double x[N+1],y[N+1],xm[20*N+1],ym[20*N+1],h[N+1],a[N+1],b[N+1],c[N+1],d[N+1];
+	//зчитування вхідних даних
+	rewind(fdata);
+	for(i=0;i<N;i++)
+	{
+		fscanf(fdata,"%Le\t%Le\t%Le",&x[i],&y[i],&h[i]);
+		//printf("[%i] %Le\t%Le\t%Le\n",i,x[i],y[i],h[i]);
+	}
+	x0=x[0];
+	long double tmp_h = h[0];
+	//функція обчислення коеф. с (метод прогонки)
+	tridiagonal_matrix_solve(y,h,N,c);
+	//знаходження коеф. a,b,d;
+	for(i=1;i<N;i++)
+	{
+		a[i]=y[i-1];
+		b[i]=(y[i]-y[i-1])/h[i]-(h[i]/3)*(c[i+1]+2*c[i]);
+		d[i]=(c[i+1]-c[i])/(3*h[i]);
+	};
+	b[N-1]=(y[N-1]-y[N-2])/h[N-1]-(2.0/3)*h[N-1]*c[N-1];
+	d[N-1]=-c[N-1]/(3*h[N-1]);
+	//знаходення значень проміжних вузлів сплайна та 
+	//запис вихідних даних у файл output.txt
+	FILE *foutput = fopen("output.txt","w");
+	//табулювання функції з кількістю кроків 20n
+	tmp_h /= 20.0;
+	for (i = 0; i < 20*N; i++)
+	{
+		xm[i] = x0+i*tmp_h;
+		ym[i] = f(xm[i]);
+	}
+	//обчислення значень сплайна у проміжних точках та похибки
+	long double s = 0,eps = 0; 
+	int j=1;
+	for (i = 0; i <= 20*(N-1); i++)
+	{
+		s=a[j]+b[j]*(xm[i]-x[j-1])+c[j]*(xm[i]-x[j-1])*(xm[i]-x[j-1])+
+					d[j]*(xm[i]-x[j-1])*(xm[i]-x[j-1])*(xm[i]-x[j-1]);
+		eps  = fabs(s-ym[i]);
+		//запис проінтерпольованої функції та похибки у файл
+		fprintf(foutput,"%Lf\t%Lf\t%Lf\t%Le\n",xm[i],ym[i],s,eps); 
+		if ((i!=0)&&((i)%20)==0) {j++;}
+	}
+	printf("Результат інтерполяції та похибка записані у файл output.txt");
+	fclose(foutput);
+	return 0;
+}
